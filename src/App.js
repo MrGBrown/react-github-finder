0cda8180c4aa3ae0ac12d77d5e8b4df3977d2399
garrett <garrett.brown2@loop.colum.edu>
2020-10-22 17:02:18 -0400

    converted class components into functional

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import "./components/Fontawesome";
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/Users/UserItem';
+import Users from './components/users/Users';
 
 class App extends Component {
   render() {
     return (
     <div className="App">
       <Navbar />
-      <UserItem />
+      <div className="container">
+        <Users />
+      </div>
     </div>
     );
 }
