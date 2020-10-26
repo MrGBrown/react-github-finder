9077e1c0b12700659891acc9d7b585afdcf55025
garrett <garrett.brown2@loop.colum.edu>
2020-10-25 21:31:13 -0400

    adding search function

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./components/Fontawesome";
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import axios from 'axios';
 
 
@@ -22,12 +23,23 @@ class App extends Component {
     
     console.log(res.data);
   }
+
+  searchUsers = async text => {
+    this.setState({ loading: true });
+    
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ users: res.data.items, loading: false });
+
+    console.log(text);
+  }
   
   render() {
     return (
     <div className="App">
       <Navbar />
       <div className="container">
+        <Search searchUsers={this.searchUsers} />
         <Users loading={this.state.loading} users={this.state.users}/>
       </div>
     </div>
